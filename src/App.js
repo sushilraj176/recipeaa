91bea6307633d843e89b55917d1fe7af4832602d
SUSHIL RAJ <70601866+sushilraj176@users.noreply.github.com>
2021-10-22 21:24:49 +0530

    Add files via upload
    updated  App.js file added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
  import React,{useEffect,useState} from "react";
 import Recipe from "./Recipe";
 import "./App.css";
-console.log(process.env);
+import Header from "./Header";
+
+
 
 
 const  App = () => {
-  
+
   const [recipes,setRecipes]=useState([]);
    const [search,setSearch]=useState("");
     const [query,setQuery]=useState("cakes");
-    const url= `https://api.edamam.com/search?q=${query}&app_id=${process.env.REACT_APP_APP_ID}&app_key=${process.env.REACT_APP_APP_KEY}`;
+    
 
-useEffect(()=>{
+
+//const APP_ID="e19e806e";
+//const APP_KEY="13b1a82fb09dc446b13b4209b01afb93";
+console.log(process.env);
+  const url= `https://api.edamam.com/search?q=${query}&app_id=${process.env.REACT_APP_APP_ID}&app_key=${process.env.REACT_APP_APP_KEY}`;
+
+  useEffect(()=>{
    getRecipes();
    // eslint-disable-next-line react-hooks/exhaustive-deps
-  } , [query]);
+  } , []);
    
   const getRecipes=async () =>{
   const response = await fetch(url);
@@ -30,14 +38,17 @@ useEffect(()=>{
    const getSearch = e =>{
      e.preventDefault();
      setQuery(search);
+     
    } ;
 
    return (
     <div className="App">
+      <Header/>
     <form onSubmit={getSearch}className="search-form">
     <input className="search-bar" type="text" value={search} onChange={updateSearch} />
     <button className="search-button" type="submit"> Search</button>
     </form>
+  
     <div className="recipes">
     {recipes.map(recipe => (
       <Recipe 
